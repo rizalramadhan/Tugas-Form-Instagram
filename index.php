f480d57761b54fe4a1dd239c241d8b368712eac3
rizalramadhan <30432179+rizalramadhan@users.noreply.github.com>
2017-10-14 00:02:37 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Instagram</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="kotak-form">
+		<div class="logo">
+			<img src="logo.png">
+		</div>
+		<div class="bawah-logo">
+			<p><b>Buat akun untuk melihat foto dan video dari teman Anda.</b></p>
+		</div>
+		<div class="fb-signin">
+			<a href="#"><b>Masuk Dengan Facebook</b></a>
+		</div>
+		<div class="atau">
+			<p><b>ATAU</b></p>
+		</div>
+		<div class="form">
+			<form action="submit.php" method="POST">
+			<input type="email" placeholder="Nomor Ponsel atau Email" name="email">
+			<input type="text" placeholder="Nama Lengkap" name="namalengkap">
+			<input type="text" placeholder="Nama Pengguna" name="username">
+			<input type="password" placeholder="Kata Sandi" name="password">
+			<input type="submit" name="submit" value="Daftar">
+			</form>
+		</div>
+		<div class="kebijakan">
+			<p>Dengan mendaftar, Anda menyetujui<b>Ketentuan</b> & <b>Kebijakan Privasi</b>kami.</p>
+		</div>
+	</div>
+</body>
+</html>
+
+<!---DilarangCopasWkwkw-->
+<!---DilarangCopasWkkk-->
